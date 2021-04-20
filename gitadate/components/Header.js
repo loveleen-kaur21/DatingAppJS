cb22e4040e99663b5cef73c593090e45090ccdac
Kenia Sandoval <ksandoval-lopez21@basecampcodingacademy.org>
2021-04-20 14:56:43 -0500

    added header

--- gitadate/components/Header.js
+++ gitadate/components/Header.js
@@ -1,11 +1,12 @@
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+import styles from "../styles/Header.module.css"
 function Header(){
     return (
-        <div>
-            <AccountBoxIcon />
-            <p>Get A Date</p>
-            <QuestionAnswerIcon />
+        <div className={styles.header}>
+            <AccountBoxIcon fontSize="large"/>
+            <p className={styles.logo}>Get A Date</p>
+            <QuestionAnswerIcon fontSize="large"/>
         </div>
     )
 }
